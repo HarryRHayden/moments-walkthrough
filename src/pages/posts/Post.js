fd5ea03c4c8ad3ef6798e6ee62e78cca3f5632a7
Harry Hayden <h_man1992@hotmail.com>
2023-01-29 21:00:04 +0000

    add dropdown options for editing and deleting posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { axiosReq } from '../../api/axiosDefaults';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq, axiosRes } from '../../api/axiosDefaults';
 import { Avatar } from '../../components/Avatar';
+import { MoreDropdown } from '../../components/MoreDropdown';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from "../../styles/Post.module.css"
 
@@ -24,6 +26,20 @@ export const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
+    const handleEdit = () => {
+        history.push(`/posts/${id}/edit`);
+    };
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/posts/${id}/`);
+            history.goBack()
+        } catch(err){
+            console.log(err);
+        }
+    };
 
     const handleLike = async () => {
         try {
@@ -39,7 +55,7 @@ export const Post = (props) => {
         } catch(err){
             console.log(err);
         }
-    }
+    };
 
     const handleUnlike = async () => {
         try {
@@ -55,7 +71,7 @@ export const Post = (props) => {
         } catch(err){
             console.log(err);
         }
-    }
+    };
 
   return (
     <Card className={styles.Post} >
@@ -66,7 +82,12 @@ export const Post = (props) => {
                 </Link>
                 <div className='d-flex align-items-center'>
                     <span>{updated_at}</span>
-                    {is_owner && postPage && "..."}
+                    {is_owner && postPage && (
+                        <MoreDropdown 
+                            handleEdit={handleEdit}
+                            handleDelete={handleDelete}
+                        />
+                    )}
                 </div>
             </Media>
         </Card.Body>
