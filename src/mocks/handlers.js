eec8ec7b799f26393fa7b2fe95136595c2520457
Harry Hayden <h_man1992@hotmail.com>
2023-01-30 21:01:45 +0000

    add initial tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,7 @@
+const baseURL = "https://front-end-test-ci.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return e
+    })
+]
